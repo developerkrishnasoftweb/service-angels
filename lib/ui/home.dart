1207da4cf54af576a8c81dc1338957c141bcc058
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2021-03-26 10:52:54 +0530

    26/03 10:52

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:service_angels/constants/pallets.dart';
 import 'package:service_angels/models/profession_card_model.dart';
 
 class Home extends StatefulWidget {
@@ -14,13 +15,15 @@ class _HomeState extends State<Home> {
         name: 'Doctors'),
     ProfessionCardInfo.asset('assets/images/icons/profession-lawyer-icon.png',
         name: 'Lawyers'),
-    ProfessionCardInfo.asset('assets/images/icons/profession-astrologers-icon.png',
+    ProfessionCardInfo.asset(
+        'assets/images/icons/profession-astrologers-icon.png',
         name: 'Astrologers'),
     ProfessionCardInfo.asset('assets/images/icons/profession-beauty-icon.png',
         name: 'Beauty'),
     ProfessionCardInfo.asset('assets/images/icons/profession-mentor-icon.png',
         name: 'Mentor'),
-    ProfessionCardInfo.asset('assets/images/icons/profession-it-consultant-icon.png',
+    ProfessionCardInfo.asset(
+        'assets/images/icons/profession-it-consultant-icon.png',
         name: 'IT Consultant')
   ];
 
@@ -39,7 +42,7 @@ class _HomeState extends State<Home> {
                   left: 0,
                   right: 0,
                   child: Container(
-                    height: size.height * 0.4,
+                    height: size.height * 0.35,
                     decoration: BoxDecoration(
                         gradient: LinearGradient(
                             begin: Alignment.topLeft,
@@ -60,8 +63,8 @@ class _HomeState extends State<Home> {
                 children: [
                   SizedBox(height: MediaQuery.of(context).padding.top),
                   Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -83,17 +86,23 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                         Container(
-                          height: 50,
-                          width: 50,
-                          padding: EdgeInsets.all(5),
+                          padding: EdgeInsets.all(3),
                           alignment: Alignment.center,
                           decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white, width: 2),
-                              shape: BoxShape.circle,
-                              image: DecorationImage(
-                                  image: AssetImage('assets/images/avatar.jpg'),
-                                  alignment: Alignment.center,
-                                  fit: BoxFit.fill)),
+                              border: Border.all(color: Colors.white),
+                              shape: BoxShape.circle),
+                          child: Container(
+                            height: 45,
+                            width: 45,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                    image:
+                                        AssetImage('assets/images/avatar.jpg'),
+                                    alignment: Alignment.center,
+                                    fit: BoxFit.fill)),
+                          ),
                         )
                       ],
                     ),
@@ -102,12 +111,13 @@ class _HomeState extends State<Home> {
                     color: Colors.white,
                     indent: dividerIndent,
                     endIndent: dividerIndent,
+                    height: 5,
                   ),
                   Align(
                       alignment: Alignment.centerLeft,
                       child: Padding(
                         padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                         child: Text("What do you want to be done ?",
                             style: TextStyle(
                                 color: Colors.white,
@@ -115,7 +125,7 @@ class _HomeState extends State<Home> {
                                 fontWeight: FontWeight.bold)),
                       )),
                   Container(
-                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                     height: 45,
                     child: TextFormField(
                       style: TextStyle(color: Colors.white),
@@ -144,6 +154,7 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   GridView.count(
+                      physics: BouncingScrollPhysics(),
                       children: professions.map((profession) {
                         return professionCard(profession);
                       }).toList(),
@@ -152,21 +163,195 @@ class _HomeState extends State<Home> {
                       crossAxisSpacing: 20,
                       shrinkWrap: true,
                       padding:
-                          EdgeInsets.symmetric(horizontal: 15, vertical: 10)),
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5)),
                   Align(
                       alignment: Alignment.centerLeft,
                       child: Padding(
                         padding:
-                        EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                         child: Text("Featured Consultants.",
                             style: TextStyle(
                                 color: Color(0xff162D63),
                                 fontSize: 16,
                                 fontWeight: FontWeight.bold)),
                       )),
-                  ListView.builder(itemBuilder: (_, index) {
-                    return ;
-                  }, itemCount: 3)
+                  Expanded(
+                    child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        physics: BouncingScrollPhysics(),
+                        itemBuilder: (_, index) {
+                          return Container(
+                            height: 130,
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 10),
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.grey.shade300,
+                                      blurRadius: 5)
+                                ]),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(3),
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              color: Color.fromRGBO(
+                                                  22, 45, 99, 0.5),
+                                              width: 2),
+                                          shape: BoxShape.circle),
+                                      child: Container(
+                                        height: 50,
+                                        width: 50,
+                                        alignment: Alignment.center,
+                                        decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            image: DecorationImage(
+                                                image: AssetImage(
+                                                    'assets/images/avatar.jpg'),
+                                                alignment: Alignment.center,
+                                                fit: BoxFit.fill)),
+                                      ),
+                                    ),
+                                    RichText(
+                                      textAlign: TextAlign.center,
+                                      text: TextSpan(children: [
+                                        WidgetSpan(
+                                            child: Icon(
+                                              Icons.star,
+                                              color: Color(0xffFFC107),
+                                              size: 24,
+                                            ),
+                                            alignment:
+                                                PlaceholderAlignment.middle),
+                                        TextSpan(
+                                            text: "4.8",
+                                            style: TextStyle(
+                                                color: Color(0xffFFC107),
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.bold)),
+                                        WidgetSpan(child: SizedBox(width: 5)),
+                                        TextSpan(
+                                            text: "(10)",
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.bold))
+                                      ]),
+                                    )
+                                  ],
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text("Dr Ganesh Shastri",
+                                            style: TextStyle(
+                                                fontSize: 18,
+                                                color: Color(0xff30BBE9),
+                                                fontWeight: FontWeight.bold)),
+                                        RichText(
+                                            text: TextSpan(
+                                                text: "Exp:20 + Years",
+                                                style: TextStyle(
+                                                    color: Color(0xff494949),
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 15),
+                                                children: [
+                                              WidgetSpan(
+                                                  child: SizedBox(width: 10)),
+                                              TextSpan(text: "Hindi, English")
+                                            ])),
+                                        Spacer(),
+                                        TextButton(
+                                          onPressed: () {},
+                                          style: ButtonStyle(
+                                              shape: MaterialStateProperty.all(
+                                                  RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              7),
+                                                      side: BorderSide(
+                                                          color: primaryColor,
+                                                          width: 2))),
+                                              padding:
+                                                  MaterialStateProperty.all(
+                                                      EdgeInsets.symmetric(
+                                                          horizontal: 10))),
+                                          child: Text("Rs.30/-Min",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 14)),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 10, vertical: 3),
+                                      alignment: Alignment.center,
+                                      decoration: BoxDecoration(
+                                          color: Color(0xff32DB08)
+                                              .withOpacity(0.2),
+                                          borderRadius:
+                                              BorderRadius.circular(5)),
+                                      child: Row(
+                                        children: [
+                                          Container(
+                                            height: 8,
+                                            width: 8,
+                                            decoration: BoxDecoration(
+                                                color: Color(0xff32DB08),
+                                                shape: BoxShape.circle),
+                                          ),
+                                          SizedBox(width: 5),
+                                          Text("online",
+                                              style: TextStyle(
+                                                  fontSize: 13,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Color(0xff32DB08)))
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 38,
+                                      width: 38,
+                                      child: TextButton(
+                                          onPressed: () {},
+                                          child: Icon(
+                                              Icons.wifi_calling_rounded,
+                                              color: Colors.white, size: 20),
+                                          style: ButtonStyle(
+                                            backgroundColor:
+                                                MaterialStateProperty.all(
+                                                    Color(0xff2FD704)),
+                                          )),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          );
+                        },
+                        itemCount: 3),
+                  )
                 ],
               )
             ],
@@ -194,19 +379,17 @@ Widget professionCard(ProfessionCardInfo profession) {
           children: [
             Image(image: profession.image),
             SizedBox(height: 5),
-            Text(profession.name, style: TextStyle(
-                color: Color(0xff162D63),
-                fontWeight: FontWeight.bold,
-                fontSize: 12
-            ))
+            Text(profession.name,
+                style: TextStyle(
+                    color: Color(0xff162D63),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12))
           ],
         ),
         decoration: BoxDecoration(
             color: Colors.white,
             boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  blurRadius: 5)
+              BoxShadow(color: Colors.black.withOpacity(0.2), blurRadius: 5)
             ],
             borderRadius: BorderRadius.circular(7))),
   );
